9cc49db71a6850b648e0ba644209265cfaf7d3c1
PingKiuHo <pingkiuho@gmail.com>
2015-10-25 21:53:22 +0800

    basic example finished

--- app/src/main/java/com/brianbridge/pulltorefreshlistview/MainActivity.java
+++ app/src/main/java/com/brianbridge/pulltorefreshlistview/MainActivity.java
@@ -1,37 +1,46 @@
 package com.brianbridge.pulltorefreshlistview;
 
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public final String TAG = MainActivity.class.getSimpleName();
+    private SwipeRefreshLayout containerSwipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        containerSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout_container);
+
+        containerSwipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        containerSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                doRefresh();
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void doRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                containerSwipeRefreshLayout.setRefreshing(false);
+                Toast.makeText(MainActivity.this, "Refresh Method ran.", Toast.LENGTH_SHORT).show();
+            }
+        }, 5000);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
